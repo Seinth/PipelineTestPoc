ecf33ad78453f5f7dc8d3dbbf65702308d757b27
Michal Wilkocki <michal.wilkocki@gmail.com>
2018-07-09 09:58:44 +0200

    Initial gradle configuration

---
+++ src/test/groovy/TestZero.groovy
@@ -0,0 +1,17 @@
+import com.lesfurets.jenkins.unit.BasePipelineTest
+import org.junit.*
+
+class TestZero extends BasePipelineTest {
+
+  @Before
+  void setup() {
+    super.setUp()
+  }
+ 
+  @Test
+  void should_execute_without_errors() throws Exception {
+    def script = loadScript("src/main/groovy/Zero.pipeline")
+    script.execute()
+    printCallStack()
+  }
+}
